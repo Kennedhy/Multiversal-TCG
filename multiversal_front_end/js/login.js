4115e68571e7c015f2d79f8f5b53a78744a1d63e
Kennedhy <kennedhysantos28@gmail.com>
2026-05-20 20:17:39 -0300

    login

--- multiversal_front_end/js/login.js
+++ multiversal_front_end/js/login.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const btnSubmit = document.getElementById('btn-submit');
   const authMessage = document.getElementById('auth-message');
   
-  const API_BASE_URL = 'http://localhost:8080/api/auth';
+  const API_BASE_URL = 'https://noemi-goateed-lavonia.ngrok-free.dev';
   let isLoginMode = true;
 
   function switchMode(isLogin) {
